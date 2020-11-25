30174c220f2a85856dae0cb32e47b11f6d1d1bbc
burmist <burm.leonid@gmail.com>
2020-11-25 17:22:27 +0100

    update

--- ex.py
+++ ex.py
@@ -8,6 +8,7 @@ Description : Simple reminder-training example.
 '''
 
 import numpy as np
+import time
 
 def test_expand_dims():
     l = [4,2,3,5]
@@ -28,8 +29,8 @@ def test_expand_dims():
     print('y          = ',y)
     print('z          = ',z)
     print('k          = ',k)
-    print(x[np.newaxis])
-    print(x[:,np.newaxis])
+    print('x[np.newaxis]      = ',x[np.newaxis])
+    print('x[:,np.newaxis]    = ',x[:,np.newaxis])
     print("np.newaxis is None : ", np.newaxis is None)
 
 def test_squeeze_dims():
@@ -37,10 +38,49 @@ def test_squeeze_dims():
     y = np.squeeze(x)
     print(x.shape)
     print(y.shape)
+
+def fill_np_arr_random(np_arr):
+    return np.append(np_arr, np.random.random(size=(n_per_job,)))
+
+def fill_np_arr_append(np_arr):
+    tic = time.time()
+    np_arr=np.append([],
+              [np.random.random(size=(n_per_job,)),
+               np.random.random(size=(n_per_job,)),
+               np.random.random(size=(n_per_job,)),
+               np.random.random(size=(n_per_job,)),
+               np.random.random(size=(n_per_job,)),
+               np.random.random(size=(n_per_job,))])
+    toc = time.time()
+    print('{} {:.2f} s'.format(len(np_arr),toc - tic))
+    time.sleep(3)
     
+def fill_np_arr_for(np_arr):
+    tic = time.time()
+    for _ in range(0,njobs):
+        np_arr = fill_np_arr_random(np_arr=np_arr)
+    toc = time.time()
+    print('{} {:.2f} s'.format(len(np_arr),toc - tic))
+    time.sleep(3)
+    
+def fill_np_arr_in_one_shot(np_arr):
+    tic = time.time()
+    np_arr=np.random.random(size=(n_per_job*njobs,))
+    toc = time.time()
+    print('{} {:.2f} s'.format(len(np_arr),toc - tic))
+    time.sleep(3)
+
 def main():
-    test_expand_dims()
-    test_squeeze_dims()
+    #test_expand_dims()
+    #test_squeeze_dims()
+
+    np_arr = np.array([])
+    #fill_np_arr_in_one_shot(np_arr)
+    #fill_np_arr_for(np_arr)
+    fill_np_arr_append(np_arr)
+    
+njobs=6
+n_per_job=int(1e8)
     
 if __name__ == "__main__":
     main()
