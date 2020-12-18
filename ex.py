19a4a746c8e3d66201d8474b71204afbc981e896
burmist <burm.leonid@gmail.com>
2020-12-18 22:41:54 +0100

    update

--- ex.py
+++ ex.py
@@ -103,7 +103,6 @@ def test_cros_dot_product():
     print('r_m.ndim  = ', r_m.ndim)
     print('r_m.shape = ', r_m.shape)
     print('r_m       = ', r_m)
-
     
 @printinfo
 def test_cros_dot_product_expand_dims():
@@ -149,6 +148,125 @@ def test_cros_dot_product_expand_dims():
     print('r_m.ndim  = ', r_m.ndim)
     print('r_m.shape = ', r_m.shape)
     print('r_m       = ', r_m)
+
+@printinfo
+def test_cros_dot_stack():
+    nPoints=3
+    x = np.array([i+1. for i in range(nPoints)])
+    y = np.array([i+4. for i in range(nPoints)])
+
+    r = np.stack((x,y))
+    rr=np.stack(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)]) for j in range(4)]))
+    rrr=np.vstack(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)]) for j in range(4)]))
+    rrrr=np.hstack(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)]) for j in range(4)]))
+    
+    print(' ')
+    print('x.ndim  = ', x.ndim)
+    print('x.shape = ', x.shape)
+    print('x       = ', x)
+
+    print(' ')
+    print('y.ndim  = ', y.ndim)
+    print('y.shape = ', y.shape)
+    print('y       = ', y)
+
+    print(' ')
+    print('r = np.stack((x,y))')
+    print('r.ndim  = ', r.ndim)
+    print('r.shape = ', r.shape)
+    print('type(r) = ', type(r))
+    print('r       = ', r)
+    
+    print(' ')
+    print('stack + list comprehension')
+    print('rr.ndim  = ', rr.ndim)
+    print('rr.shape = ', rr.shape)
+    print('type(rr) = ', type(rr))
+    print('rr       = ', rr)
+
+    print(' ')
+    print('vstack + list comprehension')
+    print('rrr.ndim  = ', rrr.ndim)
+    print('rrr.shape = ', rrr.shape)
+    print('type(rrr) = ', type(rrr))
+    print('rrr       = ', rrr)
+
+    print(' ')
+    print('hstack + list comprehension')
+    print('rrrr.ndim  = ', rrrr.ndim)
+    print('rrrr.shape = ', rrrr.shape)
+    print('type(rrrr) = ', type(rrrr))
+    print('rrrr       = ', rrrr)
+
+@printinfo
+def test_cros_dot_stack_expand_dims():
+    nPoints=3
+    x = np.expand_dims( np.array([i+1. for i in range(nPoints)]), axis=0)
+    y = np.expand_dims( np.array([i+4. for i in range(nPoints)]), axis=0)
+
+    r = np.stack((x,y))
+    #rr=np.stack(np.expand_dims(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)])) for j in range(4)]))
+    #rrr=np.vstack(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)]) for j in range(4)]))
+    #rrrr=np.hstack(np.array([np.array([i+j*nPoints+1. for i in range(nPoints)]) for j in range(4)]))
+    
+    print(' ')
+    print('x.ndim  = ', x.ndim)
+    print('x.shape = ', x.shape)
+    print('x       = ', x)
+
+    print(' ')
+    print('y.ndim  = ', y.ndim)
+    print('y.shape = ', y.shape)
+    print('y       = ', y)
+
+    print(' ')
+    print('r = np.stack((x,y))')
+    print('r.ndim  = ', r.ndim)
+    print('r.shape = ', r.shape)
+    print('type(r) = ', type(r))
+    print('r       = ',r)
+    print('r[0]    = ',r[0])
+    print('r[1]    = ',r[1])
+    print('r[0][0] ',r[0][0])
+    print('r[1][0] ',r[1][0])
+    print('r[0][0][0] ',r[0][0][0])
+    print('r[1][0][0] ',r[1][0][0])
+
+    #print(' ')
+    #print('stack + list comprehension')
+    #print('rr.ndim  = ', r.ndim)
+    #print('rr.shape = ', r.shape)
+    #print('type(rr) = ', type(rr))
+    #print('rr       = ',rr)
+    #print('rr[0]    = ',r[0])
+    #print('r[1]    = ',r[1])
+    #print('r[0][0] ',r[0][0])
+    #print('r[1][0] ',r[1][0])
+    #print('r[0][0][0] ',r[0][0][0])
+    #print('r[1][0][0] ',r[1][0][0])
+
+    '''
+    print(' ')
+    print('stack + list comprehension')
+    print('rr.ndim  = ', rr.ndim)
+    print('rr.shape = ', rr.shape)
+    print('type(rr) = ', type(rr))
+    print('rr       = ', rr)
+
+    print(' ')
+    print('vstack + list comprehension')
+    print('rrr.ndim  = ', rrr.ndim)
+    print('rrr.shape = ', rrr.shape)
+    print('type(rrr) = ', type(rrr))
+    print('rrr       = ', rrr)
+
+    print(' ')
+    print('hstack + list comprehension')
+    print('rrrr.ndim  = ', rrrr.ndim)
+    print('rrrr.shape = ', rrrr.shape)
+    print('type(rrrr) = ', type(rrrr))
+    print('rrrr       = ', rrrr)
+    '''
     
 def fill_np_arr_random(np_arr):
     return np.append(np_arr, np.random.random(size=(n_per_job,)))
@@ -184,6 +302,8 @@ def main():
     test_squeeze02_dims()
     test_cros_dot_product()
     test_cros_dot_product_expand_dims()
+    test_cros_dot_stack()
+    test_cros_dot_stack_expand_dims()
     
     #np_arr = np.array([])
     #fill_np_arr_in_one_shot(np_arr)
