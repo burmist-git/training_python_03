8ba86de6d991f8def5e55e04ff1a9e1c99ee036d
burmist <burm.leonid@gmail.com>
2020-12-18 16:45:46 +0100

    update

--- ex.py
+++ ex.py
@@ -10,6 +10,14 @@ Description : Simple reminder-training example.
 import numpy as np
 import time
 
+def printinfo(func):
+    def wrapper():
+        print("")
+        print("Simple reminder-training example. Function name : {} --> ".format(func.__name__))
+        func()
+    return wrapper
+
+@printinfo
 def test_expand_dims():
     l = [4,2,3,5]
     x = np.array(l)
@@ -33,12 +41,115 @@ def test_expand_dims():
     print('x[:,np.newaxis]    = ',x[:,np.newaxis])
     print("np.newaxis is None : ", np.newaxis is None)
 
-def test_squeeze_dims():
+@printinfo    
+def test_squeeze01_dims():
+    x = np.array([[[0], [1], [2]],[3,4,5]])
+    y = np.squeeze(x)
+    print(x.shape)
+    print(y.shape)
+    print(x)
+    print(y)
+
+@printinfo    
+def test_squeeze02_dims():
     x = np.array([[[0], [1], [2]]])
     y = np.squeeze(x)
     print(x.shape)
     print(y.shape)
+    print(x)
+    print(y)
 
+@printinfo
+def test_cros_dot_product():
+    nPoints=3
+    x = np.array([i+1. for i in range(nPoints)])
+    y = np.array([i+1. for i in range(nPoints)])
+
+    r_x_y=x*y
+    r_x_tr_y=x*np.transpose(y)
+    r_s=np.dot(x,np.transpose(y))
+    r_m=np.dot(np.transpose(y),x)
+
+    print(' ')
+    print('x.ndim  = ', x.ndim)
+    print('x.shape = ', x.shape)
+    print('x       = ', x)
+
+    print(' ')
+    print('y.ndim  = ', y.ndim)
+    print('y.shape = ', y.shape)
+    print('y       = ', y)
+
+    print(' ')
+    print('r_x_y=x*y')
+    print('r_x_y.ndim  = ', r_x_y.ndim)
+    print('r_x_y.shape = ', r_x_y.shape)
+    print('r_x_y       = ', r_x_y)
+
+    print(' ')
+    print('r_x_tr_y=x*np.transpose(y)')
+    print('r_x_tr_y.ndim  = ', r_x_tr_y.ndim)
+    print('r_x_tr_y.shape = ', r_x_tr_y.shape)
+    print('r_x_tr_y       = ', r_x_tr_y)
+
+    print(' ')
+    print('r_s=np.dot(x,np.transpose(y))')
+    print('r_s.ndim  = ', r_s.ndim)
+    print('r_s.shape = ', r_s.shape)
+    print('r_s       = ', r_s)
+
+    print(' ')
+    print('r_m=np.dot(np.transpose(y),x)')
+    print('r_m.ndim  = ', r_m.ndim)
+    print('r_m.shape = ', r_m.shape)
+    print('r_m       = ', r_m)
+
+    
+@printinfo
+def test_cros_dot_product_expand_dims():
+    nPoints=3
+    x = np.expand_dims(np.array([i+1. for i in range(nPoints)]),axis=0)
+    y = np.expand_dims(np.array([i+1. for i in range(nPoints)]),axis=0)
+
+    r_x_y=x*y
+    r_x_tr_y=x*np.transpose(y)
+    r_s=np.dot(x,np.transpose(y))
+    r_m=np.dot(np.transpose(y),x)
+    
+    print(' ')
+    print('x.ndim  = ', x.ndim)
+    print('x.shape = ', x.shape)
+    print('x       = ', x)
+
+    print(' ')
+    print('y.ndim  = ', y.ndim)
+    print('y.shape = ', y.shape)
+    print('y       = ', y)
+
+    print(' ')
+    print('r_x_y = x*y')
+    print('r_x_y.ndim  = ', r_x_y.ndim)
+    print('r_x_y.shape = ', r_x_y.shape)
+    print('r_x_y       = ', r_x_y)
+
+    print(' ')
+    print('r_x_tr_y=x*np.transpose(y)')
+    print('r_x_tr_y.ndim  = ', r_x_tr_y.ndim)
+    print('r_x_tr_y.shape = ', r_x_tr_y.shape)
+    print('r_x_tr_y       = ', r_x_tr_y)
+
+    print(' ')
+    print('r_s=np.dot(x,np.transpose(y))')
+    print('r_s.ndim  = ', r_s.ndim)
+    print('r_s.shape = ', r_s.shape)
+    print('r_s       = ', r_s)
+
+    print(' ')
+    print('r_m=np.dot(np.transpose(y),x)')
+    print('r_m.ndim  = ', r_m.ndim)
+    print('r_m.shape = ', r_m.shape)
+    print('r_m       = ', r_m)
+    
 def fill_np_arr_random(np_arr):
     return np.append(np_arr, np.random.random(size=(n_per_job,)))
 
@@ -47,14 +158,11 @@ def fill_np_arr_append(np_arr):
     np_arr=np.append([],
               [np.random.random(size=(n_per_job,)),
                np.random.random(size=(n_per_job,)),
-               np.random.random(size=(n_per_job,)),
-               np.random.random(size=(n_per_job,)),
-               np.random.random(size=(n_per_job,)),
                np.random.random(size=(n_per_job,))])
     toc = time.time()
     print('{} {:.2f} s'.format(len(np_arr),toc - tic))
     time.sleep(3)
-    
+
 def fill_np_arr_for(np_arr):
     tic = time.time()
     for _ in range(0,njobs):
@@ -62,7 +170,7 @@ def fill_np_arr_for(np_arr):
     toc = time.time()
     print('{} {:.2f} s'.format(len(np_arr),toc - tic))
     time.sleep(3)
-    
+
 def fill_np_arr_in_one_shot(np_arr):
     tic = time.time()
     np_arr=np.random.random(size=(n_per_job*njobs,))
@@ -71,16 +179,19 @@ def fill_np_arr_in_one_shot(np_arr):
     time.sleep(3)
 
 def main():
-    #test_expand_dims()
-    #test_squeeze_dims()
-
-    np_arr = np.array([])
+    test_expand_dims()
+    test_squeeze01_dims()
+    test_squeeze02_dims()
+    test_cros_dot_product()
+    test_cros_dot_product_expand_dims()
+    
+    #np_arr = np.array([])
     #fill_np_arr_in_one_shot(np_arr)
     #fill_np_arr_for(np_arr)
-    fill_np_arr_append(np_arr)
+    #fill_np_arr_append(np_arr)
     
-njobs=6
-n_per_job=int(1e8)
+njobs=2
+n_per_job=int(1e5)
     
 if __name__ == "__main__":
     main()
